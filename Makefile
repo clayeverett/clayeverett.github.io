0e08cd76b39ea8ff1e84d75f97f4c9cb474be698
Stefan Countryman <stefan.countryman@gmail.com>
2019-12-30 00:21:45 -0500

    fix some styling

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 .PHONY: build
 build:
-	./build.py 2019 2018 2017 drawings
+	./build.py 2019 2018 2017
--- build.py
+++ build.py
@@ -8,11 +8,17 @@ ENV = Environment(loader=FileSystemLoader('templates'))
 TEMPLATE = ENV.get_template('index.html')
 
 
+def title(filename):
+    year, title, medium, end = filename.replace('-', ' ').split('_')
+    dims = end.split('.')[0].replace('x', ' x ').replace('in', ' inches')
+    return f"{title} / {year} / {medium} / {dims}"
+
+
 def main():
     imagedirs = [Path(p).resolve() for p in sys.argv[1:]]
     for imdir in imagedirs:
         index = TEMPLATE.render(imdir=imdir, imagedirs=imagedirs,
-                                sorted=sorted)
+                                sorted=sorted, title=title)
         outpath = imdir/'index.html'
 
         print(f'Writing to {outpath}')
